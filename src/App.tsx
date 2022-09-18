9073dc73eb54f8b6e000b93ffff7d3290e929ec2
aaronb97 <aaron9becker7@gmail.com>
2022-09-18 17:42:45 -0400

    add speed boost and traveling to planets

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { auth } from './firebaseApp';
 
 function App() {
   const navigate = useNavigate();
-  const [userInfo, setUserInfo] = useState();
+  const [userInfo, setUserInfo] = useState<any>();
 
   const getUserInfo = useCallback(async (user: User) => {
     const data = await client.login(user);
@@ -25,11 +25,65 @@ function App() {
     return () => unsubsribe();
   }, [navigate, getUserInfo]);
 
+  if (!userInfo) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header"></header>
-      {JSON.stringify(userInfo)}
-      <button onClick={() => signOut(auth)}>Sign Out</button>
+      <div>Signed in as {userInfo.username}</div>
+      <div>Traveling to {userInfo.planet.name}</div>
+      <div>Velocities:</div>
+      <div>{userInfo.velocityX}</div>
+      <div>{userInfo.velocityY}</div>
+      <div>{userInfo.velocityZ}</div>
+      <div>Next boost: {userInfo.nextBoost}</div>
+      <div>
+        <button onClick={() => signOut(auth)}>Sign Out</button>
+      </div>
+      <div>
+        <button
+          onClick={async () => {
+            try {
+              const result = await client.speedboost();
+              setUserInfo(result?.data);
+            } catch (e) {
+              console.error(e);
+            }
+          }}
+        >
+          Speed boost
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={async () => {
+            try {
+              const result = await client.updateTravelingTo('1');
+              setUserInfo(result?.data);
+            } catch (e) {
+              console.error(e);
+            }
+          }}
+        >
+          Go to The Sun
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={async () => {
+            try {
+              const result = await client.updateTravelingTo('2');
+              setUserInfo(result?.data);
+            } catch (e) {
+              console.error(e);
+            }
+          }}
+        >
+          Go to Earth
+        </button>
+      </div>
     </div>
   );
 }
--- src/client.ts
+++ src/client.ts
@@ -24,11 +24,33 @@ class Client {
     }
 
     const token = await auth.currentUser.getIdToken();
-    axios.post(`${process.env.REACT_APP_SERVER_URL}/speedboost`, undefined, {
-      headers: {
-        authorization: `Bearer ${token}`,
+    return axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/speedboost`,
+      undefined,
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
       },
-    });
+    );
+  }
+
+  async updateTravelingTo(id: string) {
+    if (!auth.currentUser) {
+      console.error('User not defined');
+      return;
+    }
+
+    const token = await auth.currentUser.getIdToken();
+    return axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/travelingTo/${id}`,
+      undefined,
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      },
+    );
   }
 }
 
