d91dc1ff0a4fc15adc2d8d4f112000352bfcdf5a
aaronb97 <aaron9becker7@gmail.com>
2022-09-24 18:52:17 -0400

    set up react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,170 +1,34 @@
-import { onAuthStateChanged, signOut, User } from 'firebase/auth';
-import React, { useCallback, useEffect, useState } from 'react';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { useNavigate } from 'react-router-dom';
-import { client } from './client';
-import { Counter } from './components/Counter';
 import { auth } from './firebaseApp';
-import { Planet } from './types/Planet';
-import { UserData } from './types/UserData';
-import { getDateString } from './utils/getDateString';
+import { Game } from './Game';
+
+const queryClient = new QueryClient();
 
 function App() {
   const navigate = useNavigate();
-  const [userInfo, setUserInfo] = useState<UserData>();
-  const [planets, setPlanets] = useState<Planet[]>();
-  const [selectedPlanet, setSelectedPlanet] = useState<Planet>();
-
-  const getUserInfo = useCallback(async (user: User) => {
-    const data = await client.login(user);
-    setUserInfo(data?.data);
-  }, []);
-
-  const getPlanets = useCallback(async () => {
-    const data = await client.getPlanets();
-    setPlanets(data?.data);
-  }, []);
+  const [user, setUser] = useState<User | undefined>();
 
   useEffect(() => {
-    const unsubsribe = onAuthStateChanged(auth, (user) => {
-      if (!user) {
+    const unsubsribe = onAuthStateChanged(auth, (newUser) => {
+      if (!newUser) {
         navigate('/login');
       } else {
-        void getUserInfo(user);
-        void getPlanets();
+        setUser(newUser);
       }
     });
 
     return () => unsubsribe();
-  }, [navigate, getUserInfo, getPlanets]);
-
-  if (!userInfo) {
-    return <div>Loading...</div>;
-  }
-
-  const nextBoost =
-    new Date(userInfo.nextBoost).getTime() -
-    new Date(userInfo.serverTime).getTime();
-
-  const landingTime = userInfo.landingTime
-    ? new Date(userInfo.landingTime).getTime() -
-      new Date(userInfo.serverTime).getTime()
-    : undefined;
+  }, [navigate]);
 
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <div>Signed in as {userInfo.username}</div>
-      {userInfo.status === 0 && <div>Traveling to {userInfo.planet.name}</div>}
-      {userInfo.status === 1 && <div>Welcome to {userInfo.planet.name}</div>}
-      <div>Speed: {userInfo.speed.toLocaleString()} km/hour</div>
-      <Counter
-        decrement={userInfo.velocityX < 0}
-        title={'Pos X'}
-        initialValue={userInfo.positionX}
-        render={(position) => position.toFixed()}
-        interval={Math.abs((1 / userInfo.velocityX) * 60 * 60 * 1000)}
-      ></Counter>
-      <Counter
-        decrement={userInfo.velocityY < 0}
-        title={'Pos Y'}
-        initialValue={userInfo.positionY}
-        render={(position) => position.toFixed()}
-        interval={Math.abs((1 / userInfo.velocityY) * 60 * 60 * 1000)}
-      ></Counter>
-      <Counter
-        decrement={userInfo.velocityZ < 0}
-        title={'Pos Z'}
-        initialValue={userInfo.positionZ}
-        render={(position) => position.toFixed()}
-        interval={Math.abs((1 / userInfo.velocityZ) * 60 * 60 * 1000)}
-      ></Counter>
-      <div>Vel X: {userInfo.velocityX.toFixed()}</div>
-      <div>Vel Y: {userInfo.velocityY.toFixed()}</div>
-      <div>Vel Z: {userInfo.velocityZ.toFixed()}</div>
-      {userInfo.status === 0 && (
-        <Counter
-          decrement
-          title={'Next Boost'}
-          initialValue={nextBoost / 1000}
-          render={(time) =>
-            time <= 0
-              ? 'You have an available Speed Boost!'
-              : getDateString(time)
-          }
-        ></Counter>
-      )}
-      {landingTime && (
-        <Counter
-          decrement
-          title={'Landing'}
-          initialValue={landingTime / 1000}
-          render={(time) => (time <= 1 ? 'Soon...' : getDateString(time))}
-        ></Counter>
-      )}
-      <div>
-        <button
-          onClick={() => {
-            void signOut(auth);
-          }}
-        >
-          Sign Out
-        </button>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">{user && <Game user={user} />}</header>
       </div>
-      <div>
-        <button
-          onClick={() => {
-            client
-              .speedboost()
-              .then((result) => setUserInfo(result?.data))
-              .catch((e) => console.error(e));
-          }}
-        >
-          Speed boost
-        </button>
-      </div>
-      {planets && (
-        <div>
-          <select
-            onChange={(e) => {
-              setSelectedPlanet(
-                planets.find((planet) => planet.id === Number(e.target.value)),
-              );
-            }}
-            defaultValue=""
-          >
-            <option
-              value=""
-              disabled
-            >
-              Select a destination
-            </option>
-            {planets.map((planet) => (
-              <option
-                key={planet.id}
-                value={planet.id}
-                disabled={userInfo.planet.id === planet.id}
-              >
-                {planet.name}
-              </option>
-            ))}
-          </select>
-        </div>
-      )}
-      {selectedPlanet && (
-        <div>
-          <button
-            onClick={() => {
-              client
-                .updateTravelingTo(selectedPlanet.id)
-                .then((result) => setUserInfo(result?.data))
-                .catch((e) => console.error(e));
-            }}
-          >
-            Go to {selectedPlanet.name}
-          </button>
-        </div>
-      )}
-    </div>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/Game.tsx
@@ -0,0 +1,164 @@
+import { signOut, User } from 'firebase/auth';
+import { useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
+import { client } from './client';
+import { Counter } from './components/Counter';
+import { auth } from './firebaseApp';
+import { Planet } from './types/Planet';
+import { getDateString } from './utils/getDateString';
+
+interface Props {
+  user: User;
+}
+
+export function Game({ user }: Props) {
+  const [selectedPlanet, setSelectedPlanet] = useState<Planet>();
+  const queryClient = useQueryClient();
+  const { invalidateQueries } = queryClient;
+
+  const { data: userInfoData } = useQuery(
+    ['userInfo', user.uid],
+    async () => await client.login(user),
+    { staleTime: 5 * 60 * 1000, refetchInterval: 5 * 60 * 1000 },
+  );
+
+  const { data: planetData } = useQuery(
+    ['planets'],
+    async () => await client.getPlanets(),
+    { staleTime: Infinity, cacheTime: Infinity },
+  );
+
+  const planets = planetData?.data;
+
+  const userInfo = userInfoData?.data;
+
+  if (!userInfo) {
+    return <div>Loading...</div>;
+  }
+
+  const nextBoost =
+    new Date(userInfo.nextBoost).getTime() -
+    new Date(userInfo.serverTime).getTime();
+
+  const landingTime = userInfo.landingTime
+    ? new Date(userInfo.landingTime).getTime() -
+      new Date(userInfo.serverTime).getTime()
+    : undefined;
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div>Signed in as {userInfo.username}</div>
+      {userInfo.status === 0 && <div>Traveling to {userInfo.planet.name}</div>}
+      {userInfo.status === 1 && <div>Welcome to {userInfo.planet.name}</div>}
+      <div>Speed: {userInfo.speed.toLocaleString()} km/hour</div>
+      <Counter
+        decrement={userInfo.velocityX < 0}
+        title={'Pos X'}
+        initialValue={userInfo.positionX}
+        render={(position) => position.toFixed()}
+        interval={Math.abs((1 / userInfo.velocityX) * 60 * 60 * 1000)}
+      ></Counter>
+      <Counter
+        decrement={userInfo.velocityY < 0}
+        title={'Pos Y'}
+        initialValue={userInfo.positionY}
+        render={(position) => position.toFixed()}
+        interval={Math.abs((1 / userInfo.velocityY) * 60 * 60 * 1000)}
+      ></Counter>
+      <Counter
+        decrement={userInfo.velocityZ < 0}
+        title={'Pos Z'}
+        initialValue={userInfo.positionZ}
+        render={(position) => position.toFixed()}
+        interval={Math.abs((1 / userInfo.velocityZ) * 60 * 60 * 1000)}
+      ></Counter>
+      <div>Vel X: {userInfo.velocityX.toFixed()}</div>
+      <div>Vel Y: {userInfo.velocityY.toFixed()}</div>
+      <div>Vel Z: {userInfo.velocityZ.toFixed()}</div>
+      {userInfo.status === 0 && (
+        <Counter
+          decrement
+          title={'Next Boost'}
+          initialValue={nextBoost / 1000}
+          render={(time) =>
+            time <= 0
+              ? 'You have an available Speed Boost!'
+              : getDateString(time)
+          }
+        ></Counter>
+      )}
+      {landingTime && (
+        <Counter
+          decrement
+          title={'Landing'}
+          initialValue={landingTime / 1000}
+          render={(time) => (time <= 1 ? 'Soon...' : getDateString(time))}
+        ></Counter>
+      )}
+      <div>
+        <button
+          onClick={() => {
+            void signOut(auth);
+          }}
+        >
+          Sign Out
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            client
+              .speedboost()
+              .then(async () => await invalidateQueries(['userInfo']))
+              .catch((e) => console.error(e));
+          }}
+        >
+          Speed boost
+        </button>
+      </div>
+      {planets && (
+        <div>
+          <select
+            onChange={(e) => {
+              setSelectedPlanet(
+                planets.find((planet) => planet.id === Number(e.target.value)),
+              );
+            }}
+            defaultValue=""
+          >
+            <option
+              value=""
+              disabled
+            >
+              Select a destination
+            </option>
+            {planets.map((planet) => (
+              <option
+                key={planet.id}
+                value={planet.id}
+                disabled={userInfo.planet.id === planet.id}
+              >
+                {planet.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+      {selectedPlanet && (
+        <div>
+          <button
+            onClick={() => {
+              client
+                .updateTravelingTo(selectedPlanet.id)
+                .then(async () => await invalidateQueries(['userInfo']))
+                .catch((e) => console.error(e));
+            }}
+          >
+            Go to {selectedPlanet.name}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
