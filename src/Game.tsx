f769905e5d511af08a226b9722de89b2e300d670
aaronb97 <aaron9becker7@gmail.com>
2022-09-25 13:40:05 -0400

    implement dark styling

--- src/Game.tsx
+++ src/Game.tsx
@@ -190,6 +190,28 @@ export function Game({ user }: Props) {
                     setSelectedPlanet(e?.value ?? '');
                   }}
                   value={selectedOption ?? null}
+                  theme={(theme) => ({
+                    ...theme,
+                    colors: {
+                      ...theme.colors,
+                      text: 'white',
+                      neutral0: 'hsl(0, 0%, 10%)',
+                      neutral5: 'hsl(0, 0%, 20%)',
+                      neutral10: 'hsl(0, 0%, 30%)',
+                      neutral20: 'hsl(0, 0%, 40%)',
+                      neutral30: 'hsl(0, 0%, 50%)',
+                      neutral40: 'hsl(0, 0%, 60%)',
+                      neutral50: 'hsl(0, 0%, 70%)',
+                      neutral60: 'hsl(0, 0%, 80%)',
+                      neutral70: 'hsl(0, 0%, 90%)',
+                      neutral80: 'hsl(0, 0%, 95%)',
+                      neutral90: 'hsl(0, 0%, 100%)',
+                      primary: '#444',
+                      primary25: '#444',
+                      primary50: '#444',
+                      primary75: '#444',
+                    },
+                  })}
                 ></Select>
               </>
             )}
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './routes/login';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
