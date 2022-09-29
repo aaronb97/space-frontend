61fa9105195c02032b211d045c079643472c4397
aaronb97 <aaron9becker7@gmail.com>
2022-09-29 08:44:07 -0400

    Add visualizer route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { onAuthStateChanged, User } from 'firebase/auth';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { auth } from './firebase/firebaseApp';
 import { Game } from './routes/Game';
 import { Items } from './routes/items';
+const Visualizer = React.lazy(async () => await import('./routes/Visualizer'));
 
 const queryClient = new QueryClient();
 
@@ -34,6 +35,10 @@ function App() {
         <Route
           path="/items"
           element={user ? <Items user={user} /> : 'Loading...'}
+        ></Route>
+        <Route
+          path="/visualizer"
+          element={user ? <Visualizer /> : 'Loading...'}
         ></Route>
       </Routes>
     </QueryClientProvider>
