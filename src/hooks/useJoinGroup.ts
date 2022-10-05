3eddf0cf5937719444bbe82906d8e80551420355
aaronb97 <aaron9becker7@gmail.com>
2022-10-04 21:14:55 -0400

    invalid query on join group

--- src/hooks/useJoinGroup.ts
+++ src/hooks/useJoinGroup.ts
@@ -1,10 +1,12 @@
 import { useCallback } from 'react';
+import { useQueryClient } from 'react-query';
 import { useSearchParams } from 'react-router-dom';
 import { client } from '../client';
 
 export const useJoinGroup = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const join = searchParams.get('join');
+  const queryClient = useQueryClient();
 
   const joinGroup = useCallback(() => {
     if (join) {
@@ -12,6 +14,7 @@ export const useJoinGroup = () => {
         .joinGroup(join)
         .then(() => {
           console.log('successfully joined group');
+          void queryClient.invalidateQueries(['userInfo']);
         })
         .catch(() => {
           console.log('Group joining failed');
@@ -20,7 +23,7 @@ export const useJoinGroup = () => {
           setSearchParams('');
         });
     }
-  }, [join, setSearchParams]);
+  }, [join, queryClient, setSearchParams]);
 
   return joinGroup;
 };
