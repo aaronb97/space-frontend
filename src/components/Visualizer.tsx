c0ffa3aeb4ace4f400f14c9963a8942573a4780d
aaronb97 <aaron9becker7@gmail.com>
2022-10-07 13:07:15 -0400

    Fix resizing canvas

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -37,6 +37,15 @@ const camera = new THREE.PerspectiveCamera(
   50000,
 );
 
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
