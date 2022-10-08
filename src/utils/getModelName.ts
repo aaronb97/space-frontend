493eb7390147c91c5c549bddd7a04219a73e7920
aaronb97 <aaron9becker7@gmail.com>
2022-10-08 11:53:04 -0400

    Add more planet models

--- src/utils/getModelName.ts
+++ src/utils/getModelName.ts
@@ -6,11 +6,19 @@ const modelNames: Record<string, string> = {
   Venus: 'models/venus.jpg',
   Earth: 'models/earth.jpg',
   Jupiter: 'models/jupiter.jpg',
+  Io: 'models/io.jpg',
+  Callisto: 'models/callisto.jpg',
+  Europa: 'models/europa.jpg',
+  Ganymede: 'models/ganymede.jpg',
   Mars: 'models/mars.jpg',
   'The Moon': 'models/moon.jpg',
   Saturn: 'models/saturn.jpg',
+  Titan: 'models/titan.jpg',
+  Iapetus: 'models/iapetus.jpg',
   Uranus: 'models/uranus.jpg',
   Neptune: 'models/neptune.jpg',
+  Triton: 'models/triton.png',
+  Pluto: 'models/pluto.jpg',
 };
 
 export const getModelName = (planet: Planet) => {
